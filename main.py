beb68df716a9ff14d0c23b80b223f745ba0237c8
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-08-10 12:19:27 +0100

    Add command to list filaments

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import typer
+
 from prusament.core import get_filaments
 
-print(get_filaments())
+app = typer.Typer(add_completion=False)
+
+
+@app.command()
+def list():
+    '''
+    List available Prusa filaments.
+    '''
+    for filament in get_filaments():
+        print(filament)
+
+
+@app.command()
+def save():
+    print('vamos')
+
+
+if __name__ == "__main__":
+    app()
